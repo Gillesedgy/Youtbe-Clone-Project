cde33771b276e83c1e6a440f3e9a4e1cd9711c3a
Gillesedgy <edgygilles@pursuit.org>
2022-11-11 20:18:32 -0500

    trying to fix a commit issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="app">
       <h1>YOUTUBE CLONE PROJECT</h1>
-      
+{/* <Header/> or <NavBar />*/}
+{/* <Aside /> */}
+{/* <Recommended /> <Display /> */}
     </div>
   );
 }
