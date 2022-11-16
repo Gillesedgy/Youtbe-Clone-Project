083d5e55e422fc5528776eb3615f5efdbdf25a54
minagrullon <minagrullon@pursuit.org>
2022-11-16 17:07:29 -0500

    readded the .env file

--- src/fetch.js
+++ src/fetch.js
@@ -1,15 +1,14 @@
 // const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${userinput}&maxResults=15&key=${process.env.REACT_APP_API_KEY}`;
 
 export function searchFetch(input) {
-  
-// console.log(process.env.REACT_APP_API_KEY2)
-// console.log("yo")
-  let video_http = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${input}&regionCode=US&key=${process.env.REACT_APP_API_KEY2}`
+  console.log(process.env.REACT_APP_API_KEY2);
+  console.log("yo");
+  let video_http = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${input}&regionCode=US&key=${process.env.REACT_APP_API_KEY2}`;
 
   // `https://www.googleapis.com/youtube/v3/videos?key=${api_key}`;
   // let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
-  
-  // return fetch(video_http 
+
+  // return fetch(video_http
 
   // // //   + new URLSearchParams({
   // // //   // key: api_key,
@@ -20,29 +19,25 @@ export function searchFetch(input) {
   // // // })
   //  )
   //  .then(res => res.json())
-  //  .then((data)=> 
-  // //  console.log(data, "^^^", data.items[0].snippet.title) 
+  //  .then((data)=>
+  // //  console.log(data, "^^^", data.items[0].snippet.title)
   //  data.items.map((resu)=>{
   //   // resu.snippet.title
   //   console.log(resu, resu.snippet.title)
   //  })
   //  )
   //  .catch((err)=>{console.log(err)})
-  
-  
-
-
 
   // return fetch(
   //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${input}&maxResults=15&key=${process.env.REACT_APP_API_KEY}`
   // ).then((res) =>  console.log(res.json()));
 
-  //! 
+  //!
   // let video_http = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&q=${input}&regionCode=US&key=${api_key}`
   // // `https://www.googleapis.com/youtube/v3/videos?key=${api_key}`;
   // let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
-  
-  //  fetch(video_http 
+
+  //  fetch(video_http
   // //   + new URLSearchParams({
   // //   key: api_key,
   // // part: 'snippet',
@@ -55,15 +50,10 @@ export function searchFetch(input) {
   // .then(data => {
   // console.log(data.items)
 
-
   // //   // data.items.forEach(item => {
   // //   //     // getChannelIcon(item);
   // //   // })
   // })
   // .catch(err => console.log(err));
-
- 
 }
-// searchFetch()
-
-
+searchFetch();
