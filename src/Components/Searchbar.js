19f26f6f10bd7abb9df2a6c8f677bf8c533cc50f
minagrullon <minagrullon@pursuit.org>
2022-11-17 11:17:20 -0500

    added navigate to searchbar

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
-import {searchFetch} from '../fetch.js'
+import { searchFetch } from "../fetch.js";
 export default function Searchbar({ result, setResult, input, setInput }) {
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (e.target.value !== "") {
       fetchData(search);
+      navigate("/videos");
       setSearch("");
     }
   }
-  
+
   let video_http = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&regionCode=US&maxResults=25&key=${process.env.REACT_APP_API_KEY}`;
   //
   function fetchData(search) {
     fetch(video_http)
       .then((res) => res.json())
-      .then(
-        (data) => setResult(data.items)
-      )
+      .then((data) => setResult(data.items))
       .catch((err) => {
         console.log(err);
       });
