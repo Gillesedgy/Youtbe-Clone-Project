70f969562045a0b66adabd176b9b49ace50ea2a5
Gillesedgy <edgygilles@pursuit.org>
2022-11-25 14:06:57 -0500

    fixed key in searchbar

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -36,7 +36,7 @@ export default function Searchbar({
 
   let savedInput = JSON.parse(window.localStorage.getItem(search));
 
-  let video_http = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&regionCode=US&maxResults=25&key=${process.env.REACT_APP_API_KEY}`;
+  let video_http = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&regionCode=US&maxResults=25&key=${process.env.REACT_APP_API_KEY2}`;
   //
 
   function fetchData(search) {
